01b8c4e025889349c2174cd5c7c1bfd6e95ca234
almokhan <118139242+almokhan@users.noreply.github.com>
2024-08-07 15:25:40 -0300

    Update ledBlinker.ino

--- ledBlinker.ino
+++ ledBlinker.ino
@@ -1,72 +1,49 @@
-int led1 = 2;
-int led2 = 3;
-int led3 = 4;
-int led4 = 5;
-int led5 = 6;
-int button = 8;
-int ledButton = 9;
-int buttonPressed = 0;
+int buttonState = LOW;  // Estado atual do botão (LOW ou HIGH)
+int lastButtonState = LOW;  // Último estado do botão
+int buttonPin = 8;    // Pino do botão
+int pinStart = 2;     // Pino inicial dos LEDs
+int pinEnd = 6;       // Pino final dos LEDs
+int mode = 0;         // Modo de piscar LEDs: 0 - crescente, 1 - decrescente
+
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(led1, OUTPUT);
-  pinMode(led2, OUTPUT);
-  pinMode(led3, OUTPUT);
-  pinMode(led4, OUTPUT);
-  pinMode(led5, OUTPUT);
-  pinMode(button, INPUT);
-  pinMode(ledButton, OUTPUT);
+    // Configura os pinos dos LEDs como saída
+    for (int i = pinStart; i <= pinEnd; i++) {
+        pinMode(i, OUTPUT);
+    }
+    pinMode(buttonPin, INPUT);
 }
 
 void loop() {
-  
-  if (digitalRead(button) == HIGH && buttonPressed == 0){ //Le estado do botão e corre no sentido crescente
-    buttonPressed = 1;
-    digitalWrite(ledButton, HIGH);
-    delay(50)
-  };
- if (buttonPressed == 1)
- {
-  digitalWrite(ledButton, LOW);
-  digitalWrite(led5, HIGH);
-  digitalWrite(led1, LOW);
-  delay(50);
-  digitalWrite(led4, HIGH);
-  digitalWrite(led5, LOW);
-  delay(50);
-  digitalWrite(led3, HIGH);
-  digitalWrite(led4, LOW);
-  delay(50);
-  digitalWrite(led2, HIGH);
-  digitalWrite(led3, LOW);
-  delay(50);
-  digitalWrite(led1, HIGH);
-  digitalWrite(led2, LOW);
-  delay(50);
+    // Lê o estado atual do botão
+    buttonState = digitalRead(buttonPin);
 
+    // Verifica se houve uma mudança no estado do botão
+    if (buttonState != lastButtonState) {
+        if (buttonState == HIGH) {
+            // Alterna o modo de piscar LEDs quando o botão é pressionado
+            mode = (mode + 1) % 2;
+            digitalWrite(9, HIGH);
+            delay(50);  // Debounce do botão
+            digitalWrite(9, LOW);
 
- };
- if (digitalRead(button) == HIGH && buttonPressed == 1){ // Le estado do botão e corre no sentido decrescente
-    buttonPressed = 0;
-    digitalWrite(ledButton, HIGH);
-    delay(50);
-  };
- if (buttonPressed == 0){
-  digitalWrite(ledButton, LOW);
-  digitalWrite(led1, HIGH);
-  digitalWrite(led5, LOW);
-  delay(50);
-  digitalWrite(led2, HIGH);
-  digitalWrite(led1, LOW);
-  delay(50);
-  digitalWrite(led3, HIGH);
-  digitalWrite(led2, LOW);
-  delay(50);
-  digitalWrite(led4, HIGH);
-  digitalWrite(led3, LOW);
-  delay(50);
-  digitalWrite(led5, HIGH);
-  digitalWrite(led4, LOW);
-  delay(50);
+        }
+        lastButtonState = buttonState;
+    }
 
-};
+    // Pisca os LEDs de acordo com o modo atual
+    if (mode == 0) {  // Modo crescente
+        for (int i = pinStart; i <= pinEnd; i++) {
+            digitalWrite(i, HIGH);
+            delay(70);
+            digitalWrite(i, LOW);
+            delay(70);
+        }
+    } else {  // Modo decrescente
+        for (int i = pinEnd; i >= pinStart; i--) {
+            digitalWrite(i, HIGH);
+            delay(70);
+            digitalWrite(i, LOW);
+            delay(70);
+        }
+    }
 }
